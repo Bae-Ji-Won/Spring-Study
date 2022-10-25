77ad45ab89786c3ce79e744ccce7742bfca59cbf
Bae-Ji-Won <qowhxk@naver.com>
2022-10-25 11:59:46 +0900

    Factory

---
+++ git/Toby-spring-jdbc/src/main/java/dao/Factory/UserFactory.java
@@ -0,0 +1,18 @@
+package dao.Factory;
+
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class UserFactory {
+
+    @Bean           // aws 설정값을 반환해줌
+    public UserDao awsConnection(){
+        AWSConnectionMaker awsConnectionMaker = new AWSConnectionMaker();
+        UserDao userDao = new UserDao(awsConnectionMaker);      // UserDao 생성자에 넣어줌
+        return userDao;
+    }
+
+}
