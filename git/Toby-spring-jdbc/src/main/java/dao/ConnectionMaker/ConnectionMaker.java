9336dc7c620dfeacfc6e5bf38829ab33423b327f
Bae-Ji-Won <qowhxk@naver.com>
2022-10-25 11:28:08 +0900

    ConnectionMaker 인터페이스

---
+++ git/Toby-spring-jdbc/src/main/java/dao/ConnectionMaker/ConnectionMaker.java
@@ -0,0 +1,8 @@
+package dao.ConnectionMaker;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public interface ConnectionMaker {
+    Connection makeConnection() throws ClassNotFoundException, SQLException;
+}
