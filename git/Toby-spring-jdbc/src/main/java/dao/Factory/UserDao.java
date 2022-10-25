53f19a363e9943a5d4c0514e88e2aeada4d99cb1
Bae-Ji-Won <qowhxk@naver.com>
2022-10-25 14:02:09 +0900

    DAO[Factory 적용/delete,count,예외처리 추가]

--- git/Toby-spring-jdbc/src/main/java/dao/Factory/UserDao.java
+++ git/Toby-spring-jdbc/src/main/java/dao/Factory/UserDao.java
@@ -21,52 +21,96 @@ public class UserDao {
         this.connectionMaker = connectionMaker;
     }
 
-    public void add(User user) throws ClassNotFoundException {
+    public void add(User user) throws ClassNotFoundException, SQLException {
 
-        try{
-            // db 연결(호스트,이름,비밀번호)
-            Connection conn = connectionMaker.makeConnection();     // 설정들을 모아둔 메서드 호출
+        // try-catch를 통해 null일때 예외처리
 
-            PreparedStatement ps =  conn.prepareStatement("INSERT INTO users(id,name,password) VALUES(?,?,?)");
+        Connection conn = null;
+        PreparedStatement ps = null;
+        try {
+            conn = connectionMaker.makeConnection();
+
+            ps = conn.prepareStatement("INSERT INTO users(id,name,password) VALUES(?,?,?)");
             ps.setString(1,user.getId());        // mysql 테이블로 값 insert
             ps.setString(2,user.getName());
             ps.setString(3,user.getPassword());
-
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new RuntimeException(e);
+        }finally {
             ps.executeUpdate();     // ctrl + enter 즉, mysql에서 번개모양을 눌러 최신화 한다는 느낌
             ps.close();
             conn.close();
             System.out.println("데이터가 insert 됬습니다.");
-
-        }catch (SQLException e){
-            throw new RuntimeException(e);
         }
+
+
+
 
     }
 
     public User select(String id) throws SQLException, ClassNotFoundException {
 
+
+        Connection conn = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;   // 쿼리문을 저장함 insert문과 달리 excuteQuery() 사용
+        User user = null;
         try {
-            Connection conn = connectionMaker.makeConnection();
-            PreparedStatement ps = conn.prepareStatement("SELECT id,name,password FROM users WHERE id = ?");
+            conn = connectionMaker.makeConnection();
+            ps = conn.prepareStatement("SELECT id,name,password FROM users WHERE id = ?");
             ps.setString(1, id);     // id는 get(String id)로 받은 id
-            ResultSet rs = ps.executeQuery();   // 쿼리문을 저장함 insert문과 달리 excuteQuery() 사용
+            rs = ps.executeQuery();
             // rs에는 쿼리 실행 결과가 담겨져 있다. (select * from users where id = 1;)
-            rs.next();
+            rs.next();  // next() 하기 전까지는 지정하고 있는 행이 없기 때문에 next를 하여 행을 지정해야 값을 불러올 수 있음
 
             // User 생성자를 통해 쿼리문에 id값을 넣어 찾은 id, name,password 값을 저장한다.
-            User user = new User(rs.getString("id"), rs.getString("name"), rs.getString("password"));
+            return new User(rs.getString("id"), rs.getString("name"), rs.getString("password"));
+        } catch (ClassNotFoundException | SQLException  e) {
+            throw new RuntimeException(e);
+        } finally {
             rs.close();
             ps.close();
             conn.close();
-            return user;
-        }catch (SQLException e){
+        }
+
+
+    }
+
+    public void deleteAll() throws SQLException, ClassNotFoundException {
+        Connection conn = null;
+        PreparedStatement ps = null;
+        try {
+            conn = connectionMaker.makeConnection();
+            ps = conn.prepareStatement("DELETE from users");
+            ps.executeUpdate();
+        } catch (ClassNotFoundException | SQLException e) {
             throw new RuntimeException(e);
+        }finally {
+            ps.close();
+            conn.close();
+        }
+
+    }
+
+    public int getCount() throws SQLException, ClassNotFoundException {
+        Connection conn = null;
+        ResultSet result = null;        // 쿼리문 결과 저장할 변수
+        PreparedStatement ps = null;
+        try {
+            conn = connectionMaker.makeConnection();
+            result = null;
+
+            ps = conn.prepareStatement("select count(*) from users");
+            result = ps.executeQuery();     // 쿼리문 결과를 객체에 담아서 저장
+            result.next();          // result를 문자열로 변환
+            return result.getInt(1);
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new RuntimeException(e);
+        }finally {
+            result.close();
+            ps.close();
+            conn.close();
         }
     }
 
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        UserDao userDao = new UserDao();
-        userDao.add(new User("7","Ruru","1234qwer"));   // user로 값을 받아 DTO에 저장한 후 mysql로 데이터 보냄
-        System.out.println(userDao.select("1"));
-    } 
 }
