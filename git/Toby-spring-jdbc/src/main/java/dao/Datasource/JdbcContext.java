c25d2b18d4b30720ff12962aef660f0f8d15dc93
Bae-Ji-Won <qowhxk@naver.com>
2022-10-25 15:36:46 +0900

    JDBC 클래스

---
+++ git/Toby-spring-jdbc/src/main/java/dao/Datasource/JdbcContext.java
@@ -0,0 +1,37 @@
+package dao.Datasource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+// 다른 클래스에서도 사용할 수 있도록 Jdbc를 별로 클래스로 분리
+public class JdbcContext {
+
+    private DataSource dataSource;
+
+    public JdbcContext(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public void workWithStatementstrategy(StatementStrategy st){
+        Connection conn = null;
+        PreparedStatement ps = null;
+
+        try {
+            conn = dataSource.getConnection();        // DB 설정을 위해 선언
+            ps = st.makepreparedStatement(conn);    // 쿼리문을 가져오기 위해 선언
+            ps.executeUpdate();                     // 쿼리문 동작
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }finally {
+            try {
+                ps.close();
+            } catch (SQLException e) {}
+
+            try {
+                conn.close();
+            } catch (SQLException e) {}
+        }
+    }
+}
